b89af42b5141f2e299e2273ad7cc6be4491d3461
Olegs <olegs.belousovs@gmail.com>
2015-05-08 19:44:10 +0200

    Load scripts.js if the page is only single gallery.

--- functions.php
+++ functions.php
@@ -18,7 +18,9 @@ function olegs_register_files() {
 		0.1,
 		true
 	);
-	wp_enqueue_script('custom-scripts');
+	if ( is_singular( 'galleries' ) ) {
+		wp_enqueue_script('custom-scripts');
+	}
 }
 
 add_action( 'wp_enqueue_scripts', 'olegs_register_files' );
