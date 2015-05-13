6b1fb726694dc63bb5582fa743680f9242115565
Olegs <olegs.belousovs@gmail.com>
2015-05-13 11:52:05 +0200

    Added 3 media queries for conditional loading of different bg dimensions.

--- page-about.php
+++ page-about.php
@@ -8,7 +8,7 @@
 	
 	<?php while ( have_posts() ) : the_post(); ?>
 	
-		<?php // Get attachment original size image URL
+	<?php // Get attachment original size image URL
 		$thumb_full = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 		$thumb_large = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
 		$thumb_medium = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' );
