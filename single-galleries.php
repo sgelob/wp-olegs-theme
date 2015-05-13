58a2826c74e183c5f842a170cd75898a447839e4
Olegs <olegs.belousovs@gmail.com>
2015-05-13 15:38:04 +0200

    Added 3 media queries for conditional loading of bg images

--- single-galleries.php
+++ single-galleries.php
@@ -5,7 +5,7 @@
 	*/
 get_header(); ?>	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php // Get attachment original size image URL
+	<?php // Get attachment image URLs
 		$thumb_full = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 		$thumb_large = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
 		$thumb_medium = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' );
