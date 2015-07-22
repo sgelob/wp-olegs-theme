f1a97b27e2f2e86cf9ce29f7658730e8d31bfe31
Oleg <olegs.belousovs@gmail.com>
2015-07-22 12:28:25 +0200

    Name and surname minimal change.

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 	
 	/*
 	@package Olegs
+	Template Name: Quotes
 	*/
 get_header(); ?>
 	
@@ -10,8 +11,8 @@ get_header(); ?>
 		<header class="gallery-cover">
 			<div>
 			<h1 itemprop="headline"><?php the_title(); ?></h1>
-			<p>by <a rel="author" href="/about/">Olegs Belousovs</a> · 
-				<time itemprop="datePublished" datetime="<?php the_date('c'); ?>"><?php the_time('F j, Y'); ?></time>
+			<p>by <a rel="author" href="/about/">Oleg Belousov</a> · 
+				Last Updated on <time itemprop="datePublished" datetime="<?php the_date('c'); ?>"><?php the_time('F j, Y'); ?></time>
 			</p>
 			</div>
 		</header>
