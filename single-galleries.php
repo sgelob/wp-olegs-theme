23c350930de888897524c5c63298495971aa9e25
Oleg <olegs.belousovs@gmail.com>
2016-01-18 17:20:51 +0100

    Added share buttons as external include with get_template_part

--- single-galleries.php
+++ single-galleries.php
@@ -46,14 +46,7 @@ get_header(); ?>
 		<p class="excerpt"><?php echo get_the_excerpt(); ?></p>
 		<?php the_content(); ?>
 		<div class="clearfix"></div>
-		<section class="share">
-			<header>
-				<h3><?php _e('Share this gallery', 'olegs'); ?></h3>
-			</header>
-			<a class="share-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink($post->ID)); ?>" target="_blank"><?php _e('Share on Facebook', 'olegs'); ?></a>
-			<a class="share-twitter" href="https://twitter.com/intent/tweet/?text=<?php echo urlencode(get_the_title($post->ID)); ?>&url=<?php echo urlencode(wp_get_shortlink($post->ID)); ?>&via=sgelob&hashtags=photography,<?php $terms_as_text = get_the_term_list( $post->ID, 'genre', '', ', ', '' ); echo strip_tags(preg_replace('/\s+/', '', $terms_as_text)); ?>" target="_blank"><?php _e('Share on Twitter', 'olegs'); ?></a>
-			<a class="share-google" href="https://plus.google.com/share?url=<?php echo urlencode(get_permalink($post->ID)); ?>" target="_blank"><?php _e('Share on Google+', 'olegs'); ?></a>
-		</section>
+		<?php get_template_part( 'inc/share' ); ?>
 		<div class="clearfix"></div>
 		<section class="comments">
 			<header>
