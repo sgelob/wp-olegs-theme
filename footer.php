32728520f53a12be2e19ba852720fb48677e5f2a
Oleg <olegs.belousovs@gmail.com>
2016-01-24 13:13:12 +0100

    Added Google Fonts via JavaScript

--- footer.php
+++ footer.php
@@ -15,32 +15,61 @@
 </footer>
 <?php wp_footer(); ?>
 <script>
-	<?php if ( is_single() ) { ?>
-	$(document).ready(function(){
-		// FitVids.js
-		$("article").fitVids();
-	});
-	<?php } ?>
-	// Cover image div auto viewport-height
-	$(document).ready(function() {
-		resizeDiv();
-	});
-	window.onresize = function(event) {
-		resizeDiv();
-	};
-	function resizeDiv() {
-		vpw = $(window).width();
-		vph = $(window).height();
-		$('.gallery-cover').css({'height': vph + 'px'});
-		$('.page-template-page-about .gallery-cover header').css({'height': vph + 'px'});
-	}
-	// Google Analytics
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function() {
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-3011787-7', 'auto');
-	ga('send', 'pageview');	
+    <?php if ( is_single() ) { ?>
+    $(document).ready(function() {
+        // FitVids.js
+        $("article").fitVids();
+    });
+    <?php } ?>
+    // Cover image div auto viewport-height
+    $(document).ready(function() {
+        resizeDiv();
+    });
+    window.onresize = function(event) {
+        resizeDiv();
+    };
+
+    function resizeDiv() {
+        vpw = $(window).width();
+        vph = $(window).height();
+        $('.gallery-cover').css({
+            'height': vph + 'px'
+        });
+        $('.page-template-page-about .gallery-cover header').css({
+            'height': vph + 'px'
+        });
+    }
+
+    // Google Fonts
+    WebFontConfig = {
+        google: {
+            families: ['Lato:400,100,300,700:latin']
+        }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
+
+    // Google Analytics
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-3011787-7', 'auto');
+    ga('send', 'pageview');
 </script>
 </body> 
 </html>
