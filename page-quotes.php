621243db6d9bba24b162f2ad179b08daebc25649
Oleg <olegs.belousovs@gmail.com>
2016-02-02 19:11:30 +0100

    Switch URLs from HTTP to HTTPS

--- page-quotes.php
+++ page-quotes.php
@@ -6,7 +6,7 @@
 	*/
 get_header(); ?>
 
-	<article itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
+	<article itemscope="itemscope" itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
 		<header class="gallery-cover">
 			<div>
 			<h1 itemprop="headline"><?php the_title(); ?></h1>
