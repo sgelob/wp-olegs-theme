d621576f279f3dc5eeee530afe309df905fa8265
Oleg <olegs.belousovs@gmail.com>
2016-02-13 12:25:02 +0100

    Reactivated emoji

--- functions.php
+++ functions.php
@@ -31,8 +31,8 @@ remove_action('wp_head', 'wp_generator');
 
 // Remove Emoji
 
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
+// remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+// remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 // Remove Windows Live Writer link in header
 // Do Not do this if you use it
