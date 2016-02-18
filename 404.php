2a018c5823f0d8de2880770654d51046cf5bc524
Oleg <olegs.belousovs@gmail.com>
2016-02-18 13:15:11 +0100

    Replaced the redirect causing SEO issues

--- 404.php
+++ 404.php
@@ -1,10 +1,32 @@
 <?php 
+	
 	/*
 	@package Olegs
 	*/
+	get_header(); ?>
 	
-	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: ".get_bloginfo('url'));
+	<?php while ( have_posts() ) : the_post(); ?>
 
-	exit();
-?>
+<div>
+	<section class="gallery-cover">
+		<header>
+			<div>
+				<h1>
+					<span><strong><?php _e('Content not found', 'olegs'); ?></strong></span>
+					<br>
+					<span><?php _e('Sorry but I couldn\'t find the page you are looking for. Please check to make sure you\'ve typed the URL correctly.', 'olegs'); ?></span>
+				</h1>
+			</div>
+		</header>
+	</section>
+	
+	<div class="about-content">
+		<article>
+			<?php the_content(); ?>
+		</article>
+	</div>
+</div>
+
+<?php endwhile; // end of the loop. ?>
+          
+<?php get_footer(); ?>
