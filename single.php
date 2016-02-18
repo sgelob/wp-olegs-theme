c055d60c40acdccfc9acb725717bd41c2d1cb04f
Oleg <olegs.belousovs@gmail.com>
2016-02-18 11:40:03 +0100

    Added more Schema.org micro-data

--- single.php
+++ single.php
@@ -7,17 +7,18 @@ get_header(); ?>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	
 	<article itemscope="itemscope" itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
+		<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink() ?>"/>
 	<div class="about-content">
 		<header>
 			<h1 itemprop="headline"><?php the_title(); ?></h1>
-			<p><?php _e('by', 'olegs'); ?> <a rel="author" href="/about/"><?php $author = get_the_author(); echo $author; ?></a>
+			<p><?php _e('by', 'olegs'); ?> <a itemprop="author" itemscope itemtype="https://schema.org/Person" rel="author" href="/about/"><span itemprop="name"><?php $author = get_the_author(); echo $author; ?></span></a>
 				<span class="sep">·</span> 
 				<time itemprop="datePublished" datetime="<?php the_date('c'); ?>"><?php the_time('F j, Y'); ?></time>
 				<span class="sep">·</span>
 				<span><?php _e('Article in', 'olegs'); ?> <?php the_category(', ') ?></span>
 			</p>
 		</header>
-		<p class="excerpt"><?php echo get_the_excerpt(); ?></p>
+		<p class="excerpt" itemprop="description"><?php echo get_the_excerpt(); ?></p>
 		<?php the_content(); ?>
 		<div class="clearfix"></div>
 		<?php get_template_part( 'inc/share' ); ?>
