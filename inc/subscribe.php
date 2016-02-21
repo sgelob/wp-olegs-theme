d85f556f1c4bff02a7f118f84ab467c83aaa822b
Oleg <olegs.belousovs@gmail.com>
2016-02-21 15:40:18 +0100

    First commit

---
+++ inc/subscribe.php
@@ -0,0 +1,18 @@
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+	<form action="//olegs.us12.list-manage.com/subscribe/post?u=3447cd37c3c0c03c3f111a596&amp;id=f9d616b495" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<div id="mc_embed_signup_scroll">
+			<label for="mce-EMAIL">Puoi iscriverti alla mia newsletter e ricevere contenuti di valore</label>
+			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Il tuo indirizzo email" required>
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true">
+				<input type="text" name="b_3447cd37c3c0c03c3f111a596_f9d616b495" tabindex="-1" value="">
+			</div>
+			<div class="clear">
+				<input type="submit" value="Voglio iscrivermi" name="subscribe" id="mc-embedded-subscribe" class="button">
+			</div>
+		</div>
+	</form>
+</div>
+<!--End mc_embed_signup-->
