616209ceef3ba36ff8d5ba675f25811969a94bd2
Oleg <olegs.belousovs@gmail.com>
2016-02-23 11:09:37 +0100

    Disable Emoji

--- functions.php
+++ functions.php
@@ -29,10 +29,9 @@ add_action( 'wp_enqueue_scripts', 'olegs_register_files' );
 	
 remove_action('wp_head', 'wp_generator');
 
-// Remove Emoji
+// Disable Emoji
 
 function disable_wp_emojicons() {
-	if ( ! is_single() ) {
   // all actions related to emojis
   remove_action( 'admin_print_styles', 'print_emoji_styles' );
   remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
@@ -41,15 +40,8 @@ function disable_wp_emojicons() {
   remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
   remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
   remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-
-  // filter to remove TinyMCE emojis
-  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
-}
 }
 add_action( 'init', 'disable_wp_emojicons' );
-
-// remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-// remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 // Remove Windows Live Writer link in header
 // Do Not do this if you use it
