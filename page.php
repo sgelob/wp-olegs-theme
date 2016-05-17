35e3f280cd9c8dc44bf3d0859cc70c3d57889021
Oleg Belousov <oleg@OlegBookPro.local>
2016-05-17 15:35:21 +0200

    Added microformats2 and some minor fix to HTML

--- page.php
+++ page.php
@@ -7,12 +7,26 @@
 get_header(); ?>
 	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<article itemscope="itemscope" itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
+	<article itemscope="itemscope" itemtype="https://schema.org/BlogPosting" itemprop="blogPost" role="main">
+		<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink() ?>"/>
 		<header class="gallery-cover">
+			<span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+				<meta itemprop="url" content="<?php echo $thumb_url_full; ?>">
+				<meta itemprop="width" content="1620">
+				<meta itemprop="height" content="1080">
+			</span>
 			<div>
 			<h1 itemprop="headline"><?php the_title(); ?></h1>
-			<p>by <a rel="author" href="/about/">Oleg Belousov</a> · 
-				Last Updated on <time itemprop="datePublished" datetime="<?php the_date('c'); ?>"><?php the_time('F j, Y'); ?></time>
+			<p><?php _e('by', 'olegs'); ?> <a itemprop="author" itemprop="publisher" itemscope itemtype="https://schema.org/Person" rel="author" href="/about/"><span itemprop="name"><?php $author = get_the_author(); echo $author; ?></span></a>
+				<meta itemprop="dateModified" content="<?php the_modified_date('c'); ?>"/>
+				<span itemprop="publisher" itemscope itemtype="https://schema.org/Organization"/>
+					<span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+						<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/olegs-belousovs-portrait.jpg">
+						<meta itemprop="width" content="1200">
+						<meta itemprop="height" content="1200">
+    				</span>
+					<meta itemprop="name" content="<?php $author = get_the_author(); echo $author; ?>">
+				</span>
 			</p>
 			</div>
 		</header>
