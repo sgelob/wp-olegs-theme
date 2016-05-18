6686e8a7861cd0ad6720d6345a490be01756c102
Oleg Belousov <oleg@OlegBookPro.local>
2016-05-18 22:34:16 +0200

    Added fixed background image

--- 404.php
+++ 404.php
@@ -3,7 +3,23 @@
 	@package Olegs
 	*/
 get_header(); ?>
-
+	<style>
+		.gallery-cover {
+				background-image: url( https://olegs.be/wp-content/uploads/2016/02/bugs-macro-photography-61-768x512.jpg );
+			}
+		
+		@media all and (min-width: 768px) {
+			.gallery-cover {
+				background-image: url( https://olegs.be/wp-content/uploads/2016/02/bugs-macro-photography-61-1024x683.jpg );
+			}
+		}
+		
+		@media all and (min-width: 992px) {
+			.gallery-cover {
+				background-image: url( 'https://olegs.be/wp-content/uploads/2016/02/bugs-macro-photography-61.jpg' );
+			}
+		}
+	</style>
 	<article role="main">
 		<header class="gallery-cover">
 			<div>
