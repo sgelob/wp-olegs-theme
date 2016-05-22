3470605975c1e2617bb2b55c1dbc063948779b4a
Oleg Belousov <oleg@OlegBookPro.local>
2016-05-22 09:08:24 +0200

    First commit

---
+++ inc/background-image.php
@@ -0,0 +1,25 @@
+<?php // Get attachment image URLs
+  $thumb_full = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+  $thumb_large = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
+  $thumb_medium = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
+  $thumb_url_full = $thumb_full['0'];
+  $thumb_url_large = $thumb_large['0'];
+  $thumb_url_medium = $thumb_medium['0'];
+?>
+<style>
+  .gallery-cover {
+    background-image: url( <?php echo $thumb_url_medium; ?> );
+  }
+
+  @media all and (min-width: 768px) {
+    .gallery-cover {
+      background-image: url( <?php echo $thumb_url_large; ?> );
+    }
+  }
+
+  @media all and (min-width: 992px) {
+    .gallery-cover {
+      background-image: url( <?php echo $thumb_url_full; ?> );
+    }
+  }
+</style>
