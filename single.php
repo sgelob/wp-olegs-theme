d30ecd683bbbe3ded804703706a20c0a47dfc147
Oleg Belousov <oleg@OlegBookPro.local>
2016-06-08 13:16:22 +0200

    Removed link from the author meta

--- single.php
+++ single.php
@@ -1,18 +1,18 @@
-<?php 
-	
-	/*
-	@package Olegs
-	*/
-get_header(); ?>	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
+<?php
+
+    /*
+    @package Olegs
+    */
+get_header(); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 	<article itemscope="itemscope" itemtype="https://schema.org/BlogPosting" itemprop="blogPost" role="main">
 		<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink() ?>"/>
 	<div class="about-content">
 		<header>
 			<h1 itemprop="headline"><?php the_title(); ?></h1>
-			<p><a itemprop="author" itemscope itemprop="publisher" itemtype="https://schema.org/Person" rel="author" href="/about/"><span itemprop="name"><?php $author = get_the_author(); echo $author; ?></span></a>
-				<span class="sep">·</span> 
+			<p><span itemprop="name"><?php $author = get_the_author(); echo $author; ?></span>
+				<span class="sep">·</span>
 				<time itemprop="datePublished" datetime="<?php the_date('c'); ?>"><?php the_time('d/m/Y'); ?></time>
 				<meta itemprop="dateModified" content="<?php the_modified_date('c'); ?>"/>
 				<span itemprop="publisher" itemscope itemtype="https://schema.org/Organization"/>
@@ -29,7 +29,7 @@ get_header(); ?>
 		</header>
 		<p class="excerpt" itemprop="description"><?php echo get_the_excerpt(); ?></p>
 		<?php the_content(); ?>
-		<?php get_template_part( 'inc/subscribe' ); ?>
+		<?php get_template_part('inc/subscribe'); ?>
 		<div class="clearfix"></div>
 		<p class="scroll-top"><a href="#top"><?php _e('⇡ Torna su', 'olegs'); ?></a></p>
 		<div class="clearfix"></div>
@@ -37,14 +37,14 @@ get_header(); ?>
 			<header>
 				<h3><?php _e('Condividi questo articolo', 'olegs'); ?></h3>
 			</header>
-			<?php get_template_part( 'inc/share' ); ?>
+			<?php get_template_part('inc/share'); ?>
 		</section>
 		<div class="clearfix"></div>
 		<section class="comments">
 			<header>
 				<h3><?php _e('Lasciami i tuoi commenti', 'olegs'); ?></h3>
 			</header>
-			<?php comments_template( '', true ); ?>
+			<?php comments_template('', true); ?>
 		</section>
 		<div class="clearfix"></div>
 	</div>
@@ -53,5 +53,5 @@ get_header(); ?>
 <?php endwhile; else: ?>
 	<h3><?php _e('Sorry, no matched your criteria.', 'olegs'); ?></h3>
 <?php endif; wp_reset_query(); ?>
-          
+
 <?php get_footer(); ?>
