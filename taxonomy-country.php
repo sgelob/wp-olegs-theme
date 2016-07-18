8341869bd656b4346091b77539a6482a6f87db54
Olegs Belousovs <olegs.belousovs@gmail.com>
2016-07-18 21:20:28 +0200

    Better code formatting

--- taxonomy-country.php
+++ taxonomy-country.php
@@ -1,10 +1,10 @@
 <?php
-	/*
-	@package Olegs
-	*/
+    /*
+    @package Olegs
+    */
 get_header(); ?>
-<?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); ?>
-	
+<?php $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy')); ?>
+
 <section id="boxes-grid">
 	<main class="wrap" role="main" itemprop="mainContentOfPage" itemscope="itemscope" itemtype="https://schema.org/Blog">
 		<section class="genre-block">
@@ -12,9 +12,9 @@ get_header(); ?>
 				<h1><?php echo $term->name; ?></h1>
 				<p><?php echo $term->description; ?></p>
 			</header>
-			<?php query_posts(array( 'post_type'=>'galleries', 'country' => $term->slug, 'posts_per_page' => 500 )); ?>
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'inc/box' ); ?>
+			<?php query_posts(array('post_type' => 'galleries', 'country' => $term->slug, 'posts_per_page' => 500)); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php get_template_part('inc/box'); ?>
 			<?php endwhile; else: ?>
 			<h3>Sorry, no matched your criteria.</h3>
 			<?php endif; wp_reset_query(); ?>
