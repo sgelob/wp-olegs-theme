93456f4cf6e212994733c695e4fb958c6aa71403
Olegs Belousovs <olegs.belousovs@gmail.com>
2016-07-18 21:20:25 +0200

    Better code formatting

--- page.php
+++ page.php
@@ -1,38 +1,12 @@
-<?php 
-	
-	/*
-	@package Olegs
-	Template Name: Quotes
-	*/
+<?php
+
+    /*
+    @package Olegs
+    */
 get_header(); ?>
-	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php // Get attachment original size image URL
-		$thumb_full = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-		$thumb_large = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
-		$thumb_medium = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' );
-		$thumb_url_full = $thumb_full['0'];
-		$thumb_url_large = $thumb_large['0'];
-		$thumb_url_medium = $thumb_medium['0'];
-	?>
-	
-	<style>
-		.gallery-cover {
-			background-image: url( <?php echo $thumb_url_medium; ?> );
-		}
-		
-		@media all and (min-width: 768px) {
-			.gallery-cover {
-				background-image: url( <?php echo $thumb_url_large; ?> );
-			}
-		}
-		
-		@media all and (min-width: 992px) {
-			.gallery-cover {
-				background-image: url( <?php echo $thumb_url_full; ?> );
-			}
-		}
-	</style>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php get_template_part('inc/background-image'); ?>
 	<article itemscope="itemscope" itemtype="https://schema.org/BlogPosting" itemprop="blogPost" role="main">
 		<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink() ?>"/>
 		<header class="gallery-cover">
@@ -49,12 +23,12 @@ get_header(); ?>
 		<p class="excerpt"><?php echo get_the_excerpt(); ?></p>
 		<?php the_content(); ?>
 		<div class="clearfix"></div>
-		<p class="scroll-top"><a href="#top"><?php _e('⇡ Back to top', 'olegs'); ?></a></p>
+		<p class="scroll-top"><a href="#top"><?php _e('⇡ Torna su', 'olegs'); ?></a></p>
 		<div class="clearfix"></div>
 	</div>
 	</article>
 <?php endwhile; else: ?>
 	<h3>Sorry, no matched your criteria.</h3>
 <?php endif; wp_reset_query(); ?>
-          
+
 <?php get_footer(); ?>
