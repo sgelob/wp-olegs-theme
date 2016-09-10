cabaf83cfb0cde7495eb350a337bbd5a77849cc3
Olegs Belousovs <olegs.belousovs@gmail.com>
2016-09-10 16:16:06 +0200

    Removed Embeds WP js file and updated jQuery to 3.1.0

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function my_jquery_enqueue()
 {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.0.0.min.js', false, null, true);
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.1.0.min.js', false, null, true);
     
     // if (is_singular('galleries') || is_single()) {
     	wp_enqueue_script('jquery');
@@ -559,5 +559,21 @@ function jeherve_remove_all_jp_css() {
   wp_deregister_style( 'lazysizes' );
 }
 add_action('wp_print_styles', 'jeherve_remove_all_jp_css' );
+
+// Feed for Custom Post Types
+
+function myfeed_request($qv) {
+	if (isset($qv['feed']) && !isset($qv['post_type']))
+		$qv['post_type'] = array('post', 'galleries');
+	return $qv;
+}
+add_filter('request', 'myfeed_request');
+
+// Remove Embeds JavaScript
+
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
 
 ?>
