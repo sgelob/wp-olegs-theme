fa66cfb7f65d30c305726de40f7d342a6974274d
Olegs Belousovs <olegs.belousovs@gmail.com>
2016-09-10 16:15:34 +0200

    Updated code inside <img> tag for responsive images

--- inc/box.php
+++ inc/box.php
@@ -6,10 +6,8 @@
 <article class="box">
 	<a class="box-inner" href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark" itemprop="relatedLink">
 		<span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-		<img class="lazyload" src="<?php echo str_replace(".jpg", "-768x768.jpg", $thumb_url); ?>"
-			srcset="<?php echo str_replace(".jpg", "-320x320.jpg", $thumb_url); ?> 320w,
-			<?php echo str_replace(".jpg", "-480x480.jpg", $thumb_url); ?> 480w"
-			alt="<?php echo $thumb_title; ?>">
+		<img data-srcset="<?php echo str_replace(".jpg", "-320x320.jpg", $thumb_url); ?> 320w,
+			<?php echo str_replace(".jpg", "-480x480.jpg", $thumb_url); ?> 480w, <?php echo str_replace(".jpg", "-768x768.jpg", $thumb_url); ?> 768w" data-sizes="auto" data-optimumx="1.5" class="lazyload" src="<?php echo str_replace(".jpg", "-320x320.jpg", $thumb_url); ?>" alt="<?php echo $thumb_title; ?>" />
 		<meta itemprop="url" content="<?php echo str_replace(".jpg", "-768x768.jpg", $thumb_url); ?>">
 		</span>
 		<h2 itemprop="headline"><?php $theshorttitle = get_post_meta($post->ID, 'home_short_title', true);
