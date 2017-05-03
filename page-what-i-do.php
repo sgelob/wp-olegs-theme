be9e9e207b2a1e0b115599c6500e8ce82e472bc9
Olegs Belousovs <olegs.belousovs@gmail.com>
2017-05-03 21:17:45 +0200

    Changed contact link from mailto to dedicated page

--- page-what-i-do.php
+++ page-what-i-do.php
@@ -104,7 +104,7 @@
 			
 			</ul><!-- what-i-do -->
 			
-			<p class="align-center"><a class="btn" href="mailto:olegs.belousovs@gmail.com" role="button"><?php _e('Mettiamoci in contatto', 'olegs'); ?></a></p>
+			<p class="align-center"><a class="btn" href="https://olegs.be/contatti/" role="button"><?php _e('Mettiamoci in contatto', 'olegs'); ?></a></p>
 		</article>
 	</div>
 </div>
