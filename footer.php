b99032df94b5be2a12d6ece8406f3e81d4796b65
Olegs Belousovs <olegs.belousovs@gmail.com>
2018-09-29 15:03:26 +0200

    Removed Google Custom Font JS library

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<li class="footer-item"><?php echo olegs_copyright(); ?> <?php echo get_option('footercontent1'); ?></li>
 		<li class="footer-item"><?php echo get_option('footercontent2'); ?></li>
 	</ul>
-	<?php wp_nav_menu( array( 
+	<?php wp_nav_menu( array(
 		'theme_location' => 'footer-social',
 		'container' => false,
 		'link_before' => '',
@@ -16,31 +16,6 @@
 <?php wp_footer(); ?>
 
 <script>
-	performance.mark('start')
-</script>
-
-<script>
-	window.onload = function() {    
-        // Google Fonts
-		WebFontConfig = {
-        	google: {families: ['Lato:400,400italic,300,700:latin']},
-        	timeout: 10000,
-        	active: function() {
-	        	performance.mark('fonts:active');
-	        }
-    	};
-    (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-    })();
-	};
-    
-    <?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
     // Google Analytics
     (function(i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
@@ -55,8 +30,7 @@
     })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
     ga('create', 'UA-3011787-7', 'auto');
     ga('send', 'pageview');
-	<?php endif; ?>
-	
+
 	//Register a service worker
 	if ('serviceWorker' in navigator) {
 		window.addEventListener('load', function() {
@@ -75,5 +49,5 @@
 	performance.mark('end')
 </script>
 
-</body> 
+</body>
 </html>
